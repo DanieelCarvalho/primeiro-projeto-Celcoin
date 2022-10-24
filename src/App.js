03ebb57ffaee3bfb5847ff3295063c9b678fcab1
Danielscg2012@gmail.com <Danielscg2012@gmail.com>
2022-10-24 14:25:14 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Lista de Compras</h1>
+      <ul>
+        <li>Pão</li>
+        <li>Banana</li>
+        <li>Milk Shake de Doce de Leite</li>
+        <li>Nutella</li>
+        <li>Sorvete</li>
+      </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
